e5b76c9c1dc06a92223bbd74bf1939ab5fb837c0
PreetS123 <preetbhardwaj517@gmail.com>
2022-11-12 12:45:38 +0530

    minicrudapp-setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className='app'>
+         <h1>Mini Crud Application</h1>
+         <div className='CrudApp'>
+
+         </div>
+    </div>
+  );
+}
+
+export default App;
